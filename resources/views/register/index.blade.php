89eb26984c8704b55a06a7612996bbda81286d1f
rizki <54944887+vstacked@users.noreply.github.com>
2022-02-16 00:36:00 +0700

    feat: :sparkles: User Registration
    Ref https://www.youtube.com/watch?v=6sIk2sYacTw&list=PLFIM0718LjIWiihbBIq-SWPU6b6x21Q_2&index=15

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -5,23 +5,47 @@
         <div class="col-lg-5">
             <main class="form-registration">
                 <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
-                <form>
+                <form accept="/register" method="POST">
+                    @csrf
                     <div class="form-floating">
-                        <input type="text" class="form-control rounded-top" name="name" id="name" placeholder="Name">
+                        <input type="text" class="form-control rounded-top @error('name') is-invalid @enderror" name="name"
+                            id="name" placeholder="Name" required value="{{ old('name') }}">
                         <label for="name">Name</label>
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="text" class="form-control" name="username" id="username" placeholder="Username">
-                        <label for="username">Username</label>
+                        <input type="text" class="form-control @error('username') is-invalid @enderror" name="username"
+                            id="username" placeholder="Username" required value="{{ old('username') }}">
+                        <label for=" username">Username</label>
+                        @error('username')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
-                        <label for="email">Email address</label>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                            id="email" placeholder="name@example.com" required value="{{ old('email') }}">
+                        <label for=" email">Email address</label>
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="password" class="form-control rounded-bottom" name="password" id="password"
-                            placeholder="Password">
-                        <label for="password">Password</label>
+                        <input type="password" class="form-control rounded-bottom @error('password') is-invalid @enderror"
+                            name="password" id="password" placeholder="Password" required>
+                        <label for=" password">Password</label>
+                        @error('password')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Register</button>
                 </form>
