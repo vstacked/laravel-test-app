700399f365de0ec2807a73a611e57bfbad3946bf
rizki <54944887+vstacked@users.noreply.github.com>
2022-02-17 00:36:10 +0700

    feat: :sparkles: Authorization
    Ref https://www.youtube.com/watch?v=OkIa6e3SP2U&list=PLFIM0718LjIWiihbBIq-SWPU6b6x21Q_2&index=24

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DashboardPostController;
 use App\Http\Controllers\LoginController;
@@ -65,3 +66,6 @@ Route::get('/dashboard', fn () => view('dashboard.index'))->middleware('auth');
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug']);
 
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')
+    ->middleware('admin');
